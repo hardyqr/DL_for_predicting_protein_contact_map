96205f6e19ed1664d1ad228d0a1236b4a50a000e
hardyqr <liufangyu1996@gmail.com>
2018-03-06 06:36:18 -0500

    todo

--- utils.py
+++ utils.py
@@ -6,6 +6,8 @@ def seq2pairwise(seq):
     batch_size, a,b = seq.shape
     matrix = np.zeros((batch_size,a,a,b,3))
     #print(matrix.shape)
+
+    # TODO: replace loop with a np.meshgrid for efficiency
     for ba in range(0,batch_size):
         for i in range(0,a):
             for j in range(0,a):
